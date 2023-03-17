967ff7155f6fe34d3e9d2e5e87469b9024f9a743
erleibiazzio <erleibiazzio@gmail.com>
2023-03-17 10:53:48 -0300

    Seta BaseV2 como thema padrão

--- compose/common/config.d/0.main.php
+++ compose/common/config.d/0.main.php
@@ -5,13 +5,13 @@ return [
     'app.siteName' => env('SITE_NAME', 'Mapas Culturais Base Project'),
     'app.siteDescription' => i::__("O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades"),
 
-    'themes.active' => env('ACTIVE_THEME', 'MapasCulturais\Themes\BaseV1'),
+    'themes.active' => env('ACTIVE_THEME', 'MapasCulturais\Themes\BaseV2'),
 
     'app.lcode' => env('APP_LCODE', 'pt_BR,es_ES'),
 
     'namespaces' => array(
         'MapasCulturais\Themes' => THEMES_PATH,
-        'MapasCulturais\Themes\BaseV1' => THEMES_PATH . 'BaseV1/',
+        'MapasCulturais\Themes\BaseV2' => THEMES_PATH . 'BaseV2/',
         'Subsite' => THEMES_PATH . 'Subsite/',
     ),
 
