1ab52a35374fb627f3f457c84de79a3f0980080d
Welington De Lima Olegario <wolegario49@gmail.com>
2024-08-14 12:42:36 -0300

    Criação do mapeamento dos plugins

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -4,9 +4,19 @@ return [
     'plugins' => [
         'AdminLoginAsUser' => ['namespace' => 'AdminLoginAsUser'],
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
+        'Accessibility',
+        'Analytics',
         'RegistrationPayments' => [
             'namespace' => 'RegistrationPayments',
             'config' => [],
+        ],
+        'MapasBlame' => [
+            'namespace' => 'MapasBlame',
+            'config' => [
+                'request.logData.PATCH' => function ($data) {
+                    return $data;
+                },
+            ]
         ],
     ]
 ];
